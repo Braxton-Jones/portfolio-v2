bf87dd52372a414e3202af52653528df67c2025c
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2024-02-01 23:10:27 -0500

    fix

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -21,18 +21,18 @@ export default function Portfolio() {
                 transition={{ duration: 0.5, delay: 0.5 }}
                 exit={{ opacity: 0 }}
             >
-                <motion.section
+                <section
                     className={styles.navbar}
-                    initial={{ opacity: 0, y: -110 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5, delay: 1 }}
-                    exit={{ opacity: 0 }}
+                    // initial={{ opacity: 0, y: -110 }}
+                    // animate={{ opacity: 1, y: 0 }}
+                    // transition={{ duration: 0.5, delay: 1 }}
+                    // exit={{ opacity: 0 }}
                 >
                     <h1 className={styles.logo}>Braxton Jones.</h1>
                     <a href={resume} target='_blank' className={styles.resume}>
                         Resume / CV
                     </a>
-                </motion.section>
+                </section>
 
                 <section className={styles.section_one}>
                     <Hero />
